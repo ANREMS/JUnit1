3ab1c6b17a50934fd86ef7a88bb4f475ba6d97ba
Annadurai <choannadurai@gmail.com>
2023-12-09 08:28:01 +0000

    Code commit

--- src/test/java/Test1/Sam1.java
+++ src/test/java/Test1/Sam1.java
@@ -1,9 +1,10 @@
 package Test1;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Sam1 {
 
@@ -27,4 +28,33 @@ public class Sam1 {
         driver.quit();
     }
 
+    @RunWith(Parameterized.class)
+    public static class Sam4 {
+
+        String BrowserChoice;
+        public Sam4(String data)
+        {
+            BrowserChoice = data;
+        }
+        @Test
+        public void Test1()
+        {
+            if (BrowserChoice.equalsIgnoreCase("Chrome"))
+                System.out.println("Open Chrome");
+            else if (BrowserChoice.equalsIgnoreCase("Mozilla"))
+                System.out.println("Open Mozilla");
+            else
+                System.out.println("Wrong data");
+        }
+        @Parameterized.Parameters
+        public static Object parameter()
+        {
+            Object[] data = new Object[3];
+            data[0] = "Chrome";
+            data[1] = "Mozilla";
+            data[2] = "Safari";
+            return data;
+        }
+
+    }
 }
---
+++ src/test/java/Test1/Sam4.java
@@ -0,0 +1,7 @@
+package Test1;
+
+public class Sam4 {
+    public static void main(String[] args) {
+        System.out.println("Sam4 class added");
+    }
+}
